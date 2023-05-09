91bc48152bdbb2779d92a911b278cc56305a53c1
BhatiaRachna <36705069+BhatiaRachna@users.noreply.github.com>
2023-05-09 10:07:42 -0400

    Updating index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('BlockchainBites Dapp listening on port 3000!');
 });
